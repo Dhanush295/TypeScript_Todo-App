ce700a947f674296b30405d08be6bf8f21d20eea
Dhanush <dhanushg295@gmail.com>
2023-09-26 17:37:40 -0700

    updated the udate.jsx file

--- frontend/components/Update.jsx
+++ frontend/components/Update.jsx
@@ -1,7 +1,23 @@
-
+import Card from '@mui/material/Card';
+import { Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
 function Update(){
+
+    return <div>
+    <center>
+        <Card variant="outlined" style={{width:500, padding:5, margin:150, backgroundColor:"#f6f5f7"}}><br/>
+        <Typography style={{fontSize: 25, padding:10, fontFamily:"Copperplate"}}>
+            Update Todo
+        </Typography>
+        <TextField id="outlined-basic" label="Title" variant="outlined" /><br/><br/>
+        <TextField id="outlined-basic" label="Description" variant="outlined" /><br/><br/>
+        <Button variant="contained">update</Button><br/><br/>
+        </Card>
+    </center>
+</div>
 
 }
 
