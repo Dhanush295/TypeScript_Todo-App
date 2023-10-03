3dba2cc7340ee4e0740cabae973aad1fd39aa56e
Dhanush <dhanushg295@gmail.com>
2023-10-03 13:55:39 -0700

    updated db

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/users",userRouter);
 
 mongoose
-    .connect('mongodb+srv://dhanu0529:I1l1Ux1JdPh29c7s@cluster0.h5b5s2t.mongodb.net/?retryWrites=true&w=majority', {
+    .connect('Your Database url', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         dbName: 'todo'
