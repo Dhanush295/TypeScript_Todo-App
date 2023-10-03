6ef34281f55042a8b83755339a4fb1543f68a4dd
Dhanush <dhanushg295@gmail.com>
2023-10-02 18:53:37 -0700

    re-structured react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import { Component, useState } from 'react';
 import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
-import Login from '../components/Login';
-import Navbar from '../components/Navbar';
-import Signup from '../components/Signup';
-import Update from '../components/Update';
 import Todo from '../components/Todo';
 
 
@@ -15,12 +11,8 @@ function App() {
   return (
     <div style={{width:"100vw",height:"99vh" ,backgroundColor:"#ebeff3"}}>
       <Router>
-        <Navbar></Navbar>
         <Routes>
-          <Route path ={"/login"} element={<Login />}></Route>
-          <Route path ={"/signup"} element={<Signup />}></Route>
           <Route path ={"/todo"} element={<Todo/>}></Route>
-          <Route path ={"/update"} element={<Update/>}></Route>
         </Routes>
       </Router>
     </div>
